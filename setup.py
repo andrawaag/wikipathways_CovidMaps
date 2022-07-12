1febab8d00ef99053b7f808634805bfa3c22c3c0
Andra Waagmeester <andra@micelio.be>
2022-07-12 11:39:14 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wikipathways_CovidMaps',
+    version='',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='andra',
+    author_email='andra@micelio.be',
+    description=''
+)
